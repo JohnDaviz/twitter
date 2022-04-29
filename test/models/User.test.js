8dba5f32970ed32b64ec641f74e455715c00276f
Jhonatan Antonio Hernandez Sarmiento <john_daviz_hdz@hotmail.com>
2022-04-29 00:00:14 -0500

    segundo test agregado

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,19 +1,27 @@
-const User = require ('./../../app/models/User')
+const User = require('./../../app/models/User')
 
 describe("Unit Test for User class", () => {
 
     test('Create an User object', () => {
-    
-        const user = new User(1, "johndaviz", "john", "Bio", "dateCreated", "lastUpdated")
+
+        const user = new User(1, "johndaviz", "john", "Bio")
 
         //Aqui validas los resultados de ese código
         //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
-        
-      expect(user.id).toBe(1);
-      expect(user.username).toBe("johndaviz");
-      expect(user.name).toBe("john");
-      expect(user.bio).toBe("Bio");
-      expect(user.dateCreated).toBe("dateCreated");
-      expect(user.lastUpdated).toBe("lastUpdated");
+
+        expect(user.id).toBe(1);
+        expect(user.username).toBe("johndaviz");
+        expect(user.name).toBe("john");
+        expect(user.bio).toBe("Bio");
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     });
-  })
+    test('add getters', () => {
+
+        const user = new User(1,"johndaviz", "john", "Bio")
+        expect(user.getUsername).toBe("johndaviz");
+        expect(user.getBio).toBe("Bio");
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    })
+})
