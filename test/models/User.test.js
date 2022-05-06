c97849e6da9903b6b9a58015b747e0a0908cc15f
Jhonatan Antonio Hernandez Sarmiento <john_daviz_hdz@hotmail.com>
2022-05-06 12:19:15 -0500

    agrehgando setters en el test

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -24,4 +24,13 @@ describe("Unit Test for User class", () => {
         expect(user.getDateCreated).not.toBeUndefined()
         expect(user.getLastUpdated).not.toBeUndefined()
     })
+    test('add setters', () => {
+        const user = new User(1,"johndaviz", "john", "Bio")
+        user.username= "johndaviz"
+        expect(user.username).toBe("johndaviz")
+
+        user.setBio="New bio"
+        expect(user.bio).toBe("New bio")
+
+    })
 })
